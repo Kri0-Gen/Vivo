b0f460fa602c5438c9dca217710c376488218cab
xxxx <xxx@xx.xx>
2015-11-14 14:38:30 +0300

    добавили сумму заказа, кнопки и сделали форматирование

--- src/client/app/controller/order.js
+++ src/client/app/controller/order.js
@@ -17,16 +17,33 @@ define('controller/order', ['controller/main'], function(controllers){
                }
            }
        }
+       $scope.SummCost=0;
       // список блюд в заказе с их состояниями (На кухне / Готово)
-      $scope.orderDishes = [{dishId:1, Name:'Борщ', State:'', Cost:100}, {dishId:1, Name:'Борщ', State:'На кухне', Cost:100}];
+     // $scope.orderDishes = [{dishId:1, Name:'Борщ', State:'', Cost:100}, {dishId:1, Name:'Борщ', State:'На кухне', Cost:100}];
+       $scope.orderDishes = [];
       $scope.selectCategory = function(id){
          $scope.currentCat = id;
       };
        //добавление блюда в заказ
        $scope.selectDish = function(id){
            $scope.orderDishes.push({dishId: id, Name: dishHashMap[id].Name, State: '', Cost: dishHashMap[id].Cost});
+           $scope.SummCost+=dishHashMap[id].Cost;
        };
-
+        $scope.SelectDishInOrder =function(ind){
+            switch ($scope.orderDishes[ind].State){
+                case '': $scope.SummCost-=$scope.orderDishes[ind].Cost;
+                    $scope.orderDishes.splice(ind,1);break;
+                case 'На кухне': $scope.orderDishes[ind].State='Выдан'; break;
+            }
+        };
+       $scope.toKitchen =function(){
+           for (var i in $scope.orderDishes)
+           if ($scope.orderDishes[i].State==='')
+           {
+               $scope.orderDishes[i].State='На кухне';
+           }
+       };
+       $scope.toPay =function(){};
    }]);
    return 'order';
 });
