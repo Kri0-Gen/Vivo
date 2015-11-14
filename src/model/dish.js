df78e0bdb092b421809507abb4756a59562f22c6
anseko <anceko@mail.ru>
2015-11-14 12:30:00 +0300

    Commit models

--- src/model/dish.js
+++ src/model/dish.js
@@ -8,9 +8,57 @@ var mongoose=require('mongoose');
 var url =require('url');
 var db=require('../db/db');
 var dishSchem=mongoose.Schema({
-    Id:Number,
-    Name:String,
-    Cost:Number,
-    Category:String
+    Id:{
+        type:Number,
+        unique:true,
+        required:true
+    },
+    Name:{
+        type:String,
+        unique:true,
+        required:true
+    },
+    Cost:{
+        type:Number,
+        unique:true,
+        required:true
+    },
+    Category:{
+        type:Number, //ID категории  что-то вроде db.collection('categories')./.../.Id
+        unique:true,
+        required:true
+    } //свзяь
 });
-module.exports = function(){};
+var bind = function(app){
+    app.post('/dishes', function(req, res) {
+            var dishId = req.body.Id;
+            var dish = db.model('dishes', dishSchem);
+            var create = function(){
+                var dishPostedElem = new dish(req.body);
+                dishPostedElem.save();
+            };
+            if (dishId) {
+                db.collection('dishes').remove({Id: parseInt(dishId, 10)});
+                create();
+            }
+            else {
+                db.getNextSequence('dishid', function(id){
+                    req.body.Id = id;
+                  /*  req.body.Category = db.collection('categories').find(req.body.Category,function(err){
+                        if(err) throw  err;
+                    }).Id;*/
+                    create();
+                })
+            }
+
+        })
+        .get('/dishes', function(req, res){
+            var dishId = req.query['id'];
+            db.collection('dishes').find({Id:parseInt(dishId, 10)}).toArray().then(function(data){
+
+                res.json(data);
+            });
+
+        });
+};
+module.exports = bind;
--- src/model/dish_category.js
+++ src/model/dish_category.js
@@ -5,8 +5,45 @@
 var mongoose=require('mongoose');
 var url =require('url');
 var db=require('../db/db');
-var roomSchem=mongoose.Schema({
-    Id:Number,
-    Name:String
+var dish_catSchem=mongoose.Schema({
+    Id:{
+        type:Number,
+        unique:true,
+        required:true
+    },
+    Name:{
+        type:String,
+        unique:true,
+        required:true
+    }
 });
-module.exports = function(){};
+var bind = function(app){
+    app.post('/dish_cats', function(req, res) {
+            var dish_catId = req.body.Id;
+            var dish_cat = db.model('dish_cats', dish_catSchem);
+            var create = function(){
+                var dish_catPostedElem = new dish_cat(req.body);
+                dish_catPostedElem.save();
+            };
+            if (dish_catId) {
+                db.collection('dish_cats').remove({Id: parseInt(dish_catId, 10)});
+                create();
+            }
+            else {
+                db.getNextSequence('dish_catid', function(id){
+                    req.body.Id = id;
+                    create();
+                })
+            }
+
+        })
+        .get('/dishes', function(req, res){
+            var dishId = req.query['id'];
+            db.collection('dishes').find({Id:parseInt(dishId, 10)}).toArray().then(function(data){
+
+                res.json(data);
+            });
+
+        });
+};
+module.exports =bind;
