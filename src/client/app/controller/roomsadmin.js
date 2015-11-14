96e52e228861b3b891795b3c6c2c7926f8ee287d
dereka944 <qwertyk94@gmail.com>
2015-11-14 14:21:01 +0300

    поправки в залах и в афициантах

--- src/client/app/controller/roomsadmin.js
+++ src/client/app/controller/roomsadmin.js
@@ -4,33 +4,34 @@ define('controller/roomsadmin', ['controller/main', 'service/rooms'], function(c
       $scope.formHidden = true;
       $scope.addNew = function(){
          $scope.formHidden = false;
-         $scope.name = '';
-         $scope.capacity = '';
+         $scope.Name = '';
          $scope.Id=0;
-         $scope.photo = '';
+         $scope.Photo = '';
       };
       $scope.onSubmit = function(){
-         if ( $scope.name != '' )
-            roomsadminSrv.store({Id:$scope.Id, Name: $scope.name});
+         if ( $scope.Name != '' )
+            roomsadminSrv.store({Id: $scope.Id, Name: $scope.Name}).$promise.then(function(){
+               $scope.roomsadmin = roomsadminSrv.query();
+            });
          $scope.formHidden = true;
       };
       $scope.amendRoomsadmin = function(roomsadmin) {
          $scope.formHidden = false;
-         $scope.name = roomsadmin.name;
-         $scope.capacity = roomsadmin.capacity;
+         $scope.Name = roomsadmin.Name;
          $scope.Id = roomsadmin.Id;
-         $scope.photo = roomsadmin.photo;
+         $scope.Photo = roomsadmin.Photo;
       };
       $scope.onCancel = function() {
          $scope.formHidden = true;
-         $scope.name = '';
-         $scope.capacity = '';
+         $scope.Name = '';
          $scope.Id = '';
-         $scope.photo = '';
+         $scope.Photo = '';
       };
       $scope.delRoomsadmin = function(roomsadmin) {
          if (confirm('Вы точно хотите удалить официанта?'))
-            roomsadminSrv.delete({Id:roomsadmin.Id});
-      }
+            roomsadminSrv.delete({Id:roomsadmin.Id}).$promise.then(function(){
+               $scope.roomsadmin = roomsadminSrv.query();
+            });
+      };
    }]);
 });
