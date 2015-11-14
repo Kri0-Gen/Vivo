7ea4168df9abe1f416659a4acfb5dee095789c58
du.kulaevskiy <thhwitcher@gmail.com>
2015-11-14 17:25:47 +0300

    заказ блюд через БД

--- src/client/app/service/order.js
+++ src/client/app/service/order.js
@@ -4,7 +4,10 @@ define('service/order',['service/main'], function(services){
    services.factory(providerName, ['$resource', function($resource){
       return $resource(':param1/:param2/', {orderId: '@orderId'}, {
          getCategories: {method:'GET', params:{ param1: 'dish_cats', param2: 'list' }, isArray:true},
-         getDishes: {method:'GET', params:{ param1: 'dishes', param2: 'listByCat' }, isArray:false},
+         getDishes: {method:'GET', params:{ param1: 'dishes', param2: 'listByCat' }},
+         appendDishesToOrder: {method:'POST', params:{ param1: 'order', param2: 'appendToOrder' }},
+         getOrderDishes: {method:'POST', params:{ param1: 'order', param2: 'readDishes' }, isArray:true},
+         changeDishState: {method:'POST', params:{ param1: 'order', param2: 'changeDishStatus' }},
          newOrder: {method: 'POST', params: {param1: 'order', param2: 'new'}}
       });
    }]);
