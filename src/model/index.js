0002d5c58c3a8a077caa093c5054277d142f1539
as.kuzmin <the.mr.kuzzmin@gmail.com>
2015-11-14 16:01:29 +0300

    order

--- src/model/index.js
+++ src/model/index.js
@@ -42,7 +42,7 @@ module.exports = function(app){
    var filelist = fs.readdirSync(__dirname);
    for (var i = 0; i < filelist.length; i++){
       if (/.*\.js$/.test(filelist[i]) && filelist[i] != 'index.js'){
-         mod = require(path.join(__dirname, filelist[i]));
+         var mod = require(path.join(__dirname, filelist[i]));
          mod(app);
       }
    }
--- src/model/order.js
+++ src/model/order.js
@@ -34,12 +34,37 @@ var orderSchem=mongoose.Schema({
    }
 });
 
+var dishSchem=mongoose.Schema({
+   Id:{
+      type:Number,
+      unique:true,
+      required:true
+   },
+   Name:{
+      type:String,
+      unique:true,
+      required:true
+   },
+   Cost:{
+      type:Number,
+      unique:false,
+      required:true
+   },
+   Category:{
+      type:Number,
+      unique:false,
+      required:true
+   } //свзяь
+});
+
 module.exports = function(app){
    app.post('/order/new', function(req, res){
       var order = db.model('orders', orderSchem);
       var create = function(data){
          var ordePostedElem = new order(data);
-         ordePostedElem.save();
+         ordePostedElem.save(function(){
+            console.log(arguments);
+         });
       };
       db.getNextSequence('orderid', function(id){
          create({
@@ -65,17 +90,31 @@ module.exports = function(app){
        * ]
        * */
       var order = db.model('orders', orderSchem);
-      var create = function(data){
-         var ordePostedElem = new order(data);
-         ordePostedElem.save();
-      };
-      db.getNextSequence('dishorderid', function(id){
-         create({
-            Id: id,
-            Table: req.body['Table'],
-            Officiant: req.body['Officiant']
-         })
+      var orderId = parseInt(req.body.OrderId, 10);
+      order.findOne({'Id': orderId}, function(err, ord){
+         for (var i = 0; i < req.body.dishes.length; i++){
+            ord.Order.push({
+               Dish: parseInt(req.body.dishes[i], 10),
+               Status: 1
+            })
+         }
+         ord.save();
+         res.send('OK');
       });
-      res.send('OK');
+   });
+
+   app.post('/order/readDishes', function(req, res){
+      var order = db.model('orders', orderSchem);
+      var orderId = parseInt(req.body.OrderId, 10);
+      var dishes = db.model('dishes', dishSchem);
+      order.findOne({'Id': orderId}, function(err, ord){
+         var dishArr = [];
+         for( var i = 0; i < ord.Order.length; i++){
+            if (dishArr.indexOf(ord.Order[i].Dish)<0){
+               dishArr.push(ord.Order[i].Dish);
+            }
+         }
+         res.json(dishArr);
+      });
    });
 };
