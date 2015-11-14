25e0d2913230c1fe9bcef16b62c8ed7081a403df
anseko <anceko@mail.ru>
2015-11-14 13:03:11 +0300

    Commit models

--- src/model/table.js
+++ src/model/table.js
@@ -10,18 +10,43 @@ var mongoose=require('mongoose');
 var url =require('url');
 var db=require('../db/db');
 var tableSchem=mongoose.Schema({
-    Id:Number,
-    X:Number,
-    Y:Number,
-    Type:String,
-    Chairs:Number
+    Id:{
+        type:Number,
+        unique:true,
+        required:true
+    },
+    X:{
+        type:Number,
+        unique:true,
+        required:true
+    },
+    Y:{
+        type:Number,
+        unique:true,
+        required:true
+    },
+    Type:{
+        type:Number,
+        unique:true,
+        required:true
+    },
+    Chairs:{
+        type:Number,
+        unique:true,
+        required:true
+    },
+    RoomId:{
+        type:Number,
+        unique:true,
+        required:true
+            }
 });
 
 var bind = function(app){
     app.post('/tables/store', function(req, res) {
             req.body.Id = parseInt(req.body.Id || '0', 10);
             var tableId = req.body.Id;
-            var table = db.model('tables', roomSchem);
+            var table = db.model('tables', tableSchem);
             var create = function(){
                 var tablePostedElem = new table(req.body);
                 tablePostedElem.save();
